aa84759bb1c6004bd02dcfbbd82f8720e52466a5
brencerego <brencerego18@gmail.com>
2026-02-20 12:21:34 +0530

    Add files via upload

---
+++ MVC/RequestLoggingMiddleWare.cs
@@ -0,0 +1,23 @@
+﻿namespace MVC
+{
+    public class RequestLoggingMiddleWare
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggingMiddleWare(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("___ INCOMMING MESSAGE___");
+            Console.WriteLine($"Time    :  {DateTime.Now}");
+            Console.WriteLine($"Method  : {context.Request.Method}");
+            Console.WriteLine($"path    : {context.Request.Path}");
+            Console.WriteLine("------------------------------------");
+
+            await _next(context);
+        }
+    }
+}
