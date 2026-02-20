2c5944135da685f6428b22535d93c5acfbb80055
brencerego <brencerego18@gmail.com>
2026-02-20 12:07:44 +0530

    Add files via upload

---
+++ DependencyInjection/Services/MyService.cs
@@ -0,0 +1,10 @@
+﻿namespace DependencyInjection.Services
+{
+    public class MyService :IMyService
+    {
+        public string GetMessage()
+        {
+            return "Hello from Myservice using Dependency Injection";
+        }
+    }
+}
