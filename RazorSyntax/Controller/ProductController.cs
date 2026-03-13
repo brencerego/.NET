4fe9f113a168cf0e24fd48cf902575e80e5141e2
brencerego <brencerego18@gmail.com>
2026-03-13 11:46:30 +0530

    Add files via upload

---
+++ RazorSyntax/Controller/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product {Id = 1, Name ="Laptop", Price =50000 , Quantity= 5},
+                new Product {Id = 2, Name ="Phone", Price =5000 ,Quantity= 5},
+                new Product {Id = 3, Name ="Watch", Price =5000 ,Quantity= 6}
+            };
+            return View(products);
+        }
+    }
+}
